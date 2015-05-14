d0f2262d78b70f2c656de00086c67d74eb1153c2
vividhsu <vivianhustest@gmail.com>
2015-05-14 16:06:13 -0700

    add exercise 10.3

--- chapter10/chapter10/main.cpp
+++ chapter10/chapter10/main.cpp
@@ -8,16 +8,16 @@
 
 #include <iostream>
 #include <algorithm>
+#include <numeric>
 #include <vector>
 #include <list>
 #include <string>
 
 using std::count;
-
+using std::accumulate;
 using std::vector;
 using std::list;
 using std::string;
-
 using std::cin;
 using std::cout;
 using std::endl;
@@ -29,13 +29,20 @@ void ex10_1(){
 }
 
 void ex10_2(){
-    list<string> b = {"a", "the", "a", "other", "another", "a"};
-    auto cnt = count(b.begin(), b.end(), "a");
+    list<string> a = {"a", "the", "a", "other", "another", "a"};
+    auto cnt = count(a.begin(), a.end(), "a");
     cout << "how many \"a\" in the list: " << cnt << endl;
+}
+
+void ex10_3(){
+    vector<int> a = {1, 3, 5, 7, 9};
+    int sum = accumulate(a.begin(), a.end(), 0);
+    cout << "Sum of vector: " << sum << endl;
 }
 
 int main(int argc, const char * argv[]) {
     ex10_1();
     ex10_2();
+    ex10_3();
     return 0;
 }
