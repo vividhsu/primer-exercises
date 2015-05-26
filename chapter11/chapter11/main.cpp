5e9267c7ffcf4146177e04ba73d847ac23ff4631
vividhsu <vivian.xu0520@gmail.com>
2015-05-26 13:33:59 -0700

    add exercise 11.12, 11.13 and 11.14

--- chapter11/chapter11/main.cpp
+++ chapter11/chapter11/main.cpp
@@ -15,6 +15,7 @@
 #include <vector>
 #include <sstream>
 #include <list>
+#include <utility>
 
 #include "Sales_data.h"
 
@@ -30,6 +31,7 @@ using std::vector;
 using std::istringstream;
 using std::list;
 using std::multiset;
+using std::pair;
 
 
 void ex11_1(){
@@ -143,6 +145,52 @@ void ex11_11(){
     multiset<Sales_data, bool (*)(const Sales_data&, const Sales_data&)> bookstore(compareIsbn);
 }
 
+void ex11_12_13(){
+    cout << "-----ex10.12 and ex10.13----" << endl;
+    vector<pair<string, int>> pv;
+    string s;
+    int i;
+    while (cin >> s >> i){
+//        pv.push_back({s, i});
+//        pv.push_back(make_pair(s, i));
+        pv.push_back(pair<string, int>(s, i));
+    }
+    for (auto &v: pv){
+        cout << v.first << " " << v.second << "\n";
+    }
+    cout << endl;
+    
+}
+
+void ex11_14(const char* title){
+    cout << "-----ex10.14-----" << endl;
+    map<string, vector<pair<string, string>>> families;
+    ifstream input(title);
+    string line;
+    while (getline(input, line)){
+        istringstream words(line);
+        string lastName;
+        words >> lastName;
+        string firstName, birth;
+        vector<pair<string, string>> children;
+        while(words >> firstName >> birth){
+            children.push_back({firstName, birth});
+        }
+        if (families.find(lastName) == families.end()){
+            families.insert({lastName, children});
+        }
+    }
+    for (auto &f: families) {
+        cout << f.first << ": ";
+        for(auto &p: f.second){
+            cout << p.first << " " << p.second << ", ";
+        }
+        cout <<"\n";
+    }
+    cout << endl;
+    input.close();
+}
+
 int main(int argc, const char * argv[]) {
 //    ex11_1();
 //    ex11_2();
@@ -154,6 +202,8 @@ int main(int argc, const char * argv[]) {
 //    ex11_8();
 //    ex11_9();
 //    ex11_10();
-    ex11_11();
+//    ex11_11();
+//    ex11_12_13();
+    ex11_14(argv[3]);
     return 0;
 }
