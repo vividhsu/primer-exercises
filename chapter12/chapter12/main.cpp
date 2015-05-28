b3326b39138b22003dd08d8b04f256a6a8a64937
vividhsu <vivian.xu0520@gmail.com>
2015-05-28 11:22:47 -0700

    create chapter12, add exercise 12.1, 12.2, 12.3 and 12.4

--- chapter12/chapter12/main.cpp
+++ chapter12/chapter12/main.cpp
@@ -8,8 +8,41 @@
 
 #include <iostream>
 
+using std::cout;
+using std::cin;
+using std::endl;
+
+void ex12_1(){
+    cout << "-----ex12.1-----" << endl;
+    cout << "b2 is destroyed, but the elements in b2 don't.\n";
+    cout << "b1 has 4 elements, b2 has 0." << endl;
+}
+
+void ex12_2(){
+    cout << "-----ex12.2-----" << endl;
+}
+
+void ex12_3(){
+    cout << "-----ex12.3-----" << endl;
+    // https://isocpp.org/wiki/faq/const-correctness#const-member-fns
+    // http://www.douban.com/group/topic/61573279/
+    cout << "refer to https://github.com/Mooophy/Cpp-Primer/tree/master/ch12 " << endl;
+}
+
+void ex12_4(){
+    cout << "-----ex12.4-----" << endl;
+    cout << "we don't need to check if i is greater than 0 in function check because\n";
+    cout << "i is unsigned, any less than 0 input will convert to greater than 0." << endl;
+}
+
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+//    ex12_1();
+//    ex12_2();
+//    ex12_3();
+//    ex12_4();
+    
+    
     return 0;
 }
--- chapter12/chapter12/strBlob.h
+++ chapter12/chapter12/strBlob.h
@@ -9,5 +9,75 @@
 #ifndef chapter12_strBlob_h
 #define chapter12_strBlob_h
 
+#include <iostream>
+#include <vector>
+#include <exception>
+#include <string>
+#include <memory>
+#include <initialized_list>
+
+using std::vector;
+using std::string;
+using std::out_of_range;
+using std::shared_ptr;
+using std::initialized_list;
+
+class strBlob{
+public:
+    typedef vector<string>::size_type size_type;
+    //constructor
+    strBlob();
+    strBlob(initialized_list<string> il);
+    
+    size_type size() const { return data -> size(); }
+    bool empty() const { return data -> empty(); }
+    
+    void push_back(const string& t) { data -> push_back(t); }
+    void pop_back();
+    
+    string& front();
+    string& back();
+    const string& front() const;
+    const string& back() const;
+private:
+    //members
+    shared_ptr<vector<string>> data;
+    
+    void check(size_type i, const string& msg) const;
+};
+
+strBlob::strBlob(): data(make_shared<vector<string>>()) {}
+strBlob::strBlob(initialized_list<string> il): data(make_shared<vector<string>>(il)) {}
+
+void strBlob::check(size_type i, const string& msg){
+    if (i >= data -> size()) {
+        throw out_of_range(msg);
+    }
+}
+
+void strBlob::pop_back(){
+    check(0, "pop_back on empty strBlob");
+    data -> pop_back();
+}
+
+string& strBlob::front(){
+    check(0, "front on empty strBlob");
+    return data -> front();
+}
+
+const string& strBlob::front() const {
+    check(0, "front on empty strBlob");
+    return data -> front();
+}
+
+string& strBlob::back() {
+    check(0, "back on empty strBlob");
+    return data -> back();
+}
+
+const string& strBlob::back() const {
+    check(0, "back on empty strBlob");
+    return data -> back();
+}
 
 #endif
